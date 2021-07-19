7438e818edf3f8cc4d22313d9a26c3d579f9e95a
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-07-19 23:47:38 +0900

    #13 add empty set checks

--- tests/test_dataformats.py
+++ tests/test_dataformats.py
@@ -3,9 +3,22 @@ import xarray as xr
 from xarray_quantity import QuantityArray, QuantitySet
 
 
+def test_empty():
+    emptyqa = QuantityArray()
+    emptyqs = QuantitySet()
+    assert not emptyqa.unit
+    assert emptyqs.unit == {}
+
+
 def test_quantity_array():
-    qa = QuantityArray([1, 2, 3, 4, 5], unit="km/s")
+    qa = QuantityArray(
+        [1, 2, 3, 4, 5],
+        coords={"idx": [0, 1, 2, 3, 4]},
+        dims=["idx"],
+        unit="km/s",
+    )
     assert qa.unit.to_string() == "km / s"
+    assert qa.sel(idx=2).data == 3
 
 
 def test_quantity_set():
